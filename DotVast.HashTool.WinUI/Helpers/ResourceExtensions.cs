aa0d57f008025edb80fd6d0abc8f8b24c9df8c56
Kiyan Yang <KiyanYang@outlook.com>
2022-09-16 19:57:52 +0800

    修复本地化不跟随软件语言设置的问题

--- DotVast.HashTool.WinUI/Helpers/ResourceExtensions.cs
+++ DotVast.HashTool.WinUI/Helpers/ResourceExtensions.cs
@@ -4,7 +4,18 @@ namespace DotVast.HashTool.WinUI.Helpers;
 
 public static class ResourceExtensions
 {
-    private static readonly ResourceLoader s_resourceLoader = new();
+    private static readonly ResourceManager s_resourceManager;
+    private static readonly ResourceContext s_resourceContext;
+    private static readonly ResourceMap s_resourceMap;
 
-    public static string GetLocalized(this string resourceKey) => s_resourceLoader.GetString(resourceKey);
+    static ResourceExtensions()
+    {
+        s_resourceManager = new();
+        s_resourceContext = s_resourceManager.CreateResourceContext();
+        s_resourceContext.QualifierValues["Language"] = Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride;
+        s_resourceMap = s_resourceManager.MainResourceMap.GetSubtree("Resources");
+    }
+
+    public static string GetLocalized(this string resourceKey) =>
+        s_resourceMap.GetValue(resourceKey, s_resourceContext).ValueAsString;
 }
