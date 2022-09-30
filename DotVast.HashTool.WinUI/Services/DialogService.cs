0bb76fab17bd8decd17491f515af1fb18fcff7a9
Kiyan Yang <KiyanYang@outlook.com>
2022-09-30 18:01:18 +0800

    修复对话框背景 [SmokeLayerBackground] 不覆盖标题栏的问题

--- DotVast.HashTool.WinUI/Services/DialogService.cs
+++ DotVast.HashTool.WinUI/Services/DialogService.cs
@@ -1,6 +1,8 @@
+using DotVast.HashTool.WinUI.Behaviors;
 using DotVast.HashTool.WinUI.Contracts.Services;
 
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
 
 namespace DotVast.HashTool.WinUI.Services;
 
@@ -15,6 +17,8 @@ internal class DialogService : IDialogService
             CloseButtonText = closeButtonText,
             XamlRoot = App.MainWindow.Content.XamlRoot,
         };
+
+        Interaction.SetBehaviors(dialog, new() { new ContentDialogBehavior() });
 
         return await dialog.ShowAsync();
     }
