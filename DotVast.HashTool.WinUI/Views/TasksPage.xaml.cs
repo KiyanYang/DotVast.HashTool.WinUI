cc5bd6b80d4e787c34a0cf0dd5ab2a540b58da3f
Kiyan Yang <KiyanYang@outlook.com>
2022-10-05 16:47:19 +0800

    chore(TasksPage): 重新设计任务列表内容（显示附加信息）

--- DotVast.HashTool.WinUI/Views/TasksPage.xaml.cs
+++ DotVast.HashTool.WinUI/Views/TasksPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using DotVast.HashTool.WinUI.ViewModels;
+using DotVast.HashTool.WinUI.Models;
+using DotVast.HashTool.WinUI.ViewModels;
 
 using Microsoft.UI.Xaml.Controls;
 
@@ -15,5 +16,19 @@ public sealed partial class TasksPage : Page
     {
         ViewModel = App.GetService<TasksViewModel>();
         InitializeComponent();
+    }
+
+    public static string GetSecondaryInformationText(HashTask hashTask)
+    {
+        const string separator = "  |  ";
+
+        var mode = hashTask.Mode.ToString();
+        var dateTime = $"{separator}{hashTask.DateTime:HH:mm:ss}";
+        var encoding = hashTask.Mode == HashTaskMode.Text
+            ? $"{separator}{hashTask.Encoding!.WebName.ToUpper()}"
+            : string.Empty;
+        var hashNames = $"{separator}{string.Join(", ", hashTask.SelectedHashs.Select(i => i.Name))}";
+
+        return $"{mode}{dateTime}{encoding}{hashNames}";
     }
 }
