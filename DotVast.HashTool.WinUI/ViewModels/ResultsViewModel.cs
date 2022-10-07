02fd4d0da805421ad59ad479c4bf5bef93df35db
Kiyan Yang <KiyanYang@outlook.com>
2022-10-07 17:59:56 +0800

    feat(ResultsPage): 结果页面支持筛选计算内容

--- DotVast.HashTool.WinUI/ViewModels/ResultsViewModel.cs
+++ DotVast.HashTool.WinUI/ViewModels/ResultsViewModel.cs
@@ -1,13 +1,113 @@
+using DotVast.HashTool.WinUI.Contracts.ViewModels;
+using DotVast.HashTool.WinUI.Enums;
 using DotVast.HashTool.WinUI.Models;
+
+using Microsoft.UI.Xaml;
 
 namespace DotVast.HashTool.WinUI.ViewModels;
 
-public sealed partial class ResultsViewModel : ObservableRecipient
+public sealed partial class ResultsViewModel : ObservableRecipient, INavigationAware
 {
     public ResultsViewModel()
     {
     }
 
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(HashResultsFiltered))]
     private HashTask? _hashTask;
+
+    [ObservableProperty]
+    private Visibility _hashResultsVisibility;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(HashResultsFiltered))]
+    private string _hashResultsFilterByContent = string.Empty;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(HashResultsFiltered))]
+    private bool _hashResultsFilterByContentIsEnabled = false;
+
+    private IList<HashResult>? _hashResultsFiltered;
+
+    public IList<HashResult>? HashResultsFiltered
+    {
+        get
+        {
+            if (_hashResultsFilterByContentIsEnabled)
+            {
+                return HashTask?.Results?.Where(i => i.Content.Contains(HashResultsFilterByContent, StringComparison.InvariantCultureIgnoreCase)).ToList();
+            }
+            return _hashResultsFiltered;
+        }
+        private set => SetProperty(ref _hashResultsFiltered, value);
+    }
+
+    #region INavigationAware
+
+    public void OnNavigatedTo(object? parameter)
+    {
+        if (parameter is HashTask hashTask)
+        {
+            HashTask = hashTask;
+        }
+        else if (parameter != null)
+        {
+            throw new ArgumentException("导航事件参数传递的变量类型不是 HashTask");
+        }
+    }
+
+    public void OnNavigatedFrom()
+    {
+    }
+
+    #endregion INavigationAware
+
+    partial void OnHashTaskChanging(HashTask? value)
+    {
+        if (_hashTask != null)
+        {
+            _hashTask.PropertyChanged -= HashTask_PropertyChanged;
+        }
+
+        if (value != null)
+        {
+            value.PropertyChanged += HashTask_PropertyChanged;
+            HashResultsFilterByContent = string.Empty;
+            HashResultsFilterByContentIsEnabled = value.State != HashTaskState.Waiting && value.State != HashTaskState.Working;
+            HashResultsVisibility = value.Results != null ? Visibility.Visible : Visibility.Collapsed;
+            HashResultsFiltered = value.Results;
+        }
+    }
+
+    private void HashTask_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (sender is not HashTask hashTask)
+        {
+            return;
+        }
+
+        switch (e.PropertyName)
+        {
+            case nameof(HashTask.State):
+                // 当任务处于进行状态时, 禁用搜索
+                HashResultsFilterByContentIsEnabled = hashTask.State != HashTaskState.Waiting
+                    && hashTask.State != HashTaskState.Working;
+                break;
+
+            case nameof(HashTask.Results):
+                if (hashTask.Results != null)
+                {
+                    HashResultsVisibility = Visibility.Visible;
+                    HashResultsFiltered = hashTask.Results;
+                }
+                else
+                {
+                    HashResultsVisibility = Visibility.Collapsed;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
 }
--- DotVast.HashTool.WinUI/Views/ResultsPage.xaml.cs
+++ DotVast.HashTool.WinUI/Views/ResultsPage.xaml.cs
@@ -1,4 +1,3 @@
-using DotVast.HashTool.WinUI.Enums;
 using DotVast.HashTool.WinUI.Models;
 using DotVast.HashTool.WinUI.ViewModels;
 
@@ -26,26 +25,13 @@ public sealed partial class ResultsPage : Page
         base.OnNavigatedTo(e);
         if (e.Parameter is HashTask hashTask)
         {
-            ViewModel.HashTask = hashTask;
             Loaded += ResultsPage_Loaded_OnHashTaskChanged;
-        }
-        else if (e.Parameter != null)
-        {
-            throw new ArgumentException("导航事件参数传递的变量类型不是 HashTask");
         }
     }
 
     private void ResultsPage_Loaded_OnHashTaskChanged(object sender, RoutedEventArgs e)
     {
         ContentScrollViewer.ChangeView(null, 0, null);
-
-        #region {x:Bind} function to nullable, property doesn't update on null 缓解措施
-        // https://github.com/microsoft/microsoft-ui-xaml/issues/1904
-        // https://github.com/microsoft/microsoft-ui-xaml/issues/2166
-        // 修复后用法 {x:Bind helpers:XamlFunctionHelper.VisibleIfNotNull(ViewModel.HashResults)}
-        HashResultsTitle.Visibility = ViewModel.HashTask?.State != HashTaskState.Aborted
-            ? Visibility.Visible : Visibility.Collapsed;
-        #endregion
 
         Loaded -= ResultsPage_Loaded_OnHashTaskChanged;
     }
