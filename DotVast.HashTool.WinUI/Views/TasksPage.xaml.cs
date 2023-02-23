4268cce4366599924d99d7179ab67ef93b71cf85
Kiyan Yang <KiyanYang@outlook.com>
2023-02-23 14:09:47 +0800

    chore: 任务页面新增显示/隐藏选择按钮

--- DotVast.HashTool.WinUI/Views/TasksPage.xaml.cs
+++ DotVast.HashTool.WinUI/Views/TasksPage.xaml.cs
@@ -6,14 +6,28 @@ namespace DotVast.HashTool.WinUI.Views;
 
 public sealed partial class TasksPage : Page
 {
+    public AuxiliaryObject Auxiliary { get; }
     public TasksViewModel ViewModel { get; }
 
     public TasksPage()
     {
+        Auxiliary = new();
         ViewModel = App.GetService<TasksViewModel>();
+        Resources.AddExpression(() => Auxiliary);
         Resources.AddExpression(() => ViewModel.SaveCommand);
         Resources.AddExpression(() => ViewModel.ShowResultCommand);
         Resources.AddExpression(() => Localization.Command_Save_Tip);
         InitializeComponent();
+    }
+
+    public sealed partial class AuxiliaryObject : ObservableObject
+    {
+        [ObservableProperty]
+        private bool _isSelectEnabled;
+
+        public AuxiliaryObject()
+        {
+            _isSelectEnabled = false;
+        }
     }
 }
