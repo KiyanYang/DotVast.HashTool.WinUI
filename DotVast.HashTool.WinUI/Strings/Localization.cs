c9099e455591fcb0beeb761234e403b46cf634bf
Kiyan Yang <KiyanYang@outlook.com>
2023-03-24 13:17:19 +0800

    chore: 更新依赖 DotVast.Toolkit.StringResource 至 0.5.0

--- DotVast.HashTool.WinUI/Strings/Localization.cs
+++ DotVast.HashTool.WinUI/Strings/Localization.cs
@@ -2,25 +2,25 @@ using DotVast.Toolkit.StringResource;
 
 namespace DotVast.HashTool.WinUI.Strings;
 
-[StringResource("../zh-Hans/Resources.resw", "GetLocalized()", "DotVast.HashTool.WinUI.Helpers")]
+[StringResource("../zh-Hans/Resources.resw", "public static string {0} => \"{0}\".GetLocalized();", "using DotVast.HashTool.WinUI.Helpers;")]
 internal static partial class Localization
 {
 
 }
 
-[StringResource("../zh-Hans/Dialogs.resw", "GetLocalized(\"Dialogs\")", "DotVast.HashTool.WinUI.Helpers")]
+[StringResource("../zh-Hans/Dialogs.resw", "public static string {0} => \"{0}\".GetLocalized(\"Dialogs\");", "using DotVast.HashTool.WinUI.Helpers;")]
 internal static partial class LocalizationDialog
 {
 
 }
 
-[StringResource("../zh-Hans/Common.resw", "GetLocalized(\"Common\")", "DotVast.HashTool.WinUI.Helpers")]
+[StringResource("../zh-Hans/Common.resw", "public static string {0} => \"{0}\".GetLocalized(\"Common\");", "using DotVast.HashTool.WinUI.Helpers;")]
 internal static partial class LocalizationCommon
 {
 
 }
 
-[StringResource("../zh-Hans/Enums.resw", "GetLocalized(\"Enums\")", "DotVast.HashTool.WinUI.Helpers")]
+[StringResource("../zh-Hans/Enums.resw", "public static string {0} => \"{0}\".GetLocalized(\"Enums\");", "using DotVast.HashTool.WinUI.Helpers;")]
 internal static partial class LocalizationEnum
 {
 
