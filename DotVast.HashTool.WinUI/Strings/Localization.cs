9c3a0f74ac5df3fffd4f47c434170af330bcfbb7
Kiyan Yang <KiyanYang@outlook.com>
2023-03-24 15:04:53 +0800

    chore: 优化 Localization

--- DotVast.HashTool.WinUI/Strings/Localization.cs
+++ DotVast.HashTool.WinUI/Strings/Localization.cs
@@ -1,27 +1,60 @@
 using DotVast.Toolkit.StringResource;
 
+using Microsoft.Windows.ApplicationModel.Resources;
+
+using Windows.Globalization;
+
 namespace DotVast.HashTool.WinUI.Strings;
 
-[StringResource("../zh-Hans/Resources.resw", "public static string {0} => \"{0}\".GetLocalized();", "using DotVast.HashTool.WinUI.Helpers;")]
-internal static partial class Localization
+internal abstract class BaseLocalization
 {
+    private const string LanguageQualifier = "Language";
 
+    private static readonly ResourceManager s_resourceManager;
+    private static readonly ResourceContext s_resourceContext;
+    private static readonly ResourceMap s_resourceMap;
+
+    private static ResourceContext? s_tmpResourceContext;
+
+    static BaseLocalization()
+    {
+        s_resourceManager = new ResourceManager();
+        s_resourceContext = s_resourceManager.CreateResourceContext();
+        s_resourceContext.QualifierValues[LanguageQualifier] = ApplicationLanguages.PrimaryLanguageOverride;
+        s_resourceMap = s_resourceManager.MainResourceMap;
+    }
+
+    internal static string GetLocalized(string subtreeId, string resourceKey) =>
+        s_resourceMap.GetSubtree(subtreeId).GetValue(resourceKey, s_resourceContext).ValueAsString;
+
+    internal static string GetLocalized(string subtreeId, string resourceKey, string language)
+    {
+        s_tmpResourceContext ??= s_resourceManager.CreateResourceContext();
+        s_tmpResourceContext.QualifierValues[LanguageQualifier] = language;
+        return s_resourceMap.GetSubtree(subtreeId).GetValue(resourceKey, s_tmpResourceContext).ValueAsString;
+    }
 }
 
-[StringResource("../zh-Hans/Dialogs.resw", "public static string {0} => \"{0}\".GetLocalized(\"Dialogs\");", "using DotVast.HashTool.WinUI.Helpers;")]
-internal static partial class LocalizationDialog
+[StringResource("../zh-Hans/" + SubtreeId + ".resw", "public static string {0} => __{0} ??= GetLocalized(\"" + SubtreeId + "\", \"{0}\");\nprivate static string? __{0};")]
+internal sealed partial class Localization : BaseLocalization
 {
-
+    internal const string SubtreeId = "Resources";
 }
 
-[StringResource("../zh-Hans/Common.resw", "public static string {0} => \"{0}\".GetLocalized(\"Common\");", "using DotVast.HashTool.WinUI.Helpers;")]
-internal static partial class LocalizationCommon
+[StringResource("../zh-Hans/" + SubtreeId + ".resw", "public static string {0} => __{0} ??= GetLocalized(\"" + SubtreeId + "\", \"{0}\");\nprivate static string? __{0};")]
+internal sealed partial class LocalizationCommon : BaseLocalization
 {
-
+    internal const string SubtreeId = "Common";
 }
 
-[StringResource("../zh-Hans/Enums.resw", "public static string {0} => \"{0}\".GetLocalized(\"Enums\");", "using DotVast.HashTool.WinUI.Helpers;")]
-internal static partial class LocalizationEnum
+[StringResource("../zh-Hans/" + SubtreeId + ".resw", "public static string {0} => __{0} ??= GetLocalized(\"" + SubtreeId + "\", \"{0}\");\nprivate static string? __{0};")]
+internal sealed partial class LocalizationDialog : BaseLocalization
 {
+    internal const string SubtreeId = "Dialogs";
+}
 
+[StringResource("../zh-Hans/" + SubtreeId + ".resw", "public static string {0} => __{0} ??= GetLocalized(\"" + SubtreeId + "\", \"{0}\");\nprivate static string? __{0};")]
+internal sealed partial class LocalizationEnum : BaseLocalization
+{
+    internal const string SubtreeId = "Enums";
 }
--- DotVast.HashTool.WinUI/Usings.cs
+++ DotVast.HashTool.WinUI/Usings.cs
@@ -8,7 +8,6 @@ global using DotVast.HashTool.WinUI.Strings;
 global using static CommunityToolkit.Mvvm.Messaging.IMessengerExtensions;
 global using static DotVast.HashTool.WinUI.Helpers.Log;
 global using static DotVast.HashTool.WinUI.Helpers.ResourceDictionaryExtensions;
-global using static DotVast.HashTool.WinUI.Helpers.ResourceExtensions;
 
 global using JsonContext = DotVast.HashTool.WinUI.Helpers.JsonContext;
 global using JsonContext2 = DotVast.HashTool.WinUI.Helpers.JsonContext2;
