ec8a98c53a32284772a94ff1d58198e34edf7f6b
Kiyan Yang <KiyanYang@outlook.com>
2024-02-26 14:35:18 +0800

    chore: 简化 StringResource 模板

--- src/DotVast.HashTool.WinUI/Strings/Localization.cs
+++ src/DotVast.HashTool.WinUI/Strings/Localization.cs
@@ -6,25 +6,25 @@ using DotVast.Toolkit.StringResource;
 
 namespace DotVast.HashTool.WinUI.Strings;
 
-[StringResource("../zh-Hans/" + SubtreeId + ".resw", "public static string {0} => __{0} ??= GetLocalized(\"" + SubtreeId + "\", \"{0}\");\nprivate static string? __{0};")]
+[StringResource($"""../zh-Hans/{SubtreeId}.resw""", $$$"""public static string {0} {{ get; }} = GetLocalized("{{{SubtreeId}}}", "{0}");""")]
 internal sealed partial class Localization : BaseLocalization
 {
     internal const string SubtreeId = "Resources";
 }
 
-[StringResource("../zh-Hans/" + SubtreeId + ".resw", "public static string {0} => __{0} ??= GetLocalized(\"" + SubtreeId + "\", \"{0}\");\nprivate static string? __{0};")]
+[StringResource($"""../zh-Hans/{SubtreeId}.resw""", $$$"""public static string {0} {{ get; }} = GetLocalized("{{{SubtreeId}}}", "{0}");""")]
 internal sealed partial class LocalizationCommon : BaseLocalization
 {
     internal const string SubtreeId = "Common";
 }
 
-[StringResource("../zh-Hans/" + SubtreeId + ".resw", "public static string {0} => __{0} ??= GetLocalized(\"" + SubtreeId + "\", \"{0}\");\nprivate static string? __{0};")]
+[StringResource($"""../zh-Hans/{SubtreeId}.resw""", $$$"""public static string {0} {{ get; }} = GetLocalized("{{{SubtreeId}}}", "{0}");""")]
 internal sealed partial class LocalizationPopup : BaseLocalization
 {
     internal const string SubtreeId = "Popups";
 }
 
-[StringResource("../zh-Hans/" + SubtreeId + ".resw", "public static string {0} => __{0} ??= GetLocalized(\"" + SubtreeId + "\", \"{0}\");\nprivate static string? __{0};")]
+[StringResource($"""../zh-Hans/{SubtreeId}.resw""", $$$"""public static string {0} {{ get; }} = GetLocalized("{{{SubtreeId}}}", "{0}");""")]
 internal sealed partial class LocalizationEnum : BaseLocalization
 {
     internal const string SubtreeId = "Enums";
