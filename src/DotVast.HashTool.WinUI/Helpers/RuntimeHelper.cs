da8f0cca9a26f88a1139171116eb42143c30d3ed
Kiyan Yang <KiyanYang@outlook.com>
2024-08-15 17:11:35 +0800

    chore: 清理代码, 移除 nuget.config

--- src/DotVast.HashTool.WinUI/Helpers/RuntimeHelper.cs
+++ src/DotVast.HashTool.WinUI/Helpers/RuntimeHelper.cs
@@ -7,12 +7,6 @@ namespace DotVast.HashTool.WinUI.Helpers;
 
 public sealed class RuntimeHelper
 {
-    static RuntimeHelper()
-    {
-        ThreadPool.GetMaxThreads(out s_defaultMaxWorkerThreads, out s_defaultMaxCompletionPortThreads);
-        ThreadPool.GetMinThreads(out s_defaultMinWorkerThreads, out s_defaultMinCompletionPortThreads);
-    }
-
     #region AppVersion
 
     private static Version? s_appVersion;
@@ -24,20 +18,4 @@ public sealed class RuntimeHelper
     }
 
     #endregion AppVersion
-
-    #region Threads
-
-    public static int DefaultMaxWorkerThreads => s_defaultMaxWorkerThreads;
-    private static readonly int s_defaultMaxWorkerThreads;
-
-    public static int DefaultMaxCompletionPortThreads => s_defaultMaxCompletionPortThreads;
-    private static readonly int s_defaultMaxCompletionPortThreads;
-
-    public static int DefaultMinWorkerThreads => s_defaultMinWorkerThreads;
-    private static readonly int s_defaultMinWorkerThreads;
-
-    public static int DefaultMinCompletionPortThreads => s_defaultMinCompletionPortThreads;
-    private static readonly int s_defaultMinCompletionPortThreads;
-
-    #endregion Threads
 }
